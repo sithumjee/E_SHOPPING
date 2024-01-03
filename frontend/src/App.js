89b945ef0c523d8231251fba0cd8b5ac0bbb2781
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-03 15:57:37 +0530

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,31 @@
 
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/women" element={<ShopCategory />} />
+          <Route path="/kids" element={<ShopCategory />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/product/:id\" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
