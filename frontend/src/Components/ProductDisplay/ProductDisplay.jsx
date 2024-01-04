c5513605a389aa4e0eea2609299dede19745d751
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-05 00:03:21 +0530

    product display css

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -30,35 +30,35 @@ const ProductDisplay = (props) => {
           <img src={star_dull_icon} alt="" />
           <p>(122)</p>
         </div>
-      </div>
 
-      <div className="display-right-price">
-        <div className="rightPriceOld">${product.old_price}</div>
-        <div className="rightPriceNew">${product.new_price}</div>
-      </div>
-
-      <div className="display-right-description">
-        <p>{product.description}</p>
-      </div>
-
-      <div className="display-right-size">
-        <h3>Size</h3>
-        <div className="sizeList">
-          <div className="sizeList-item">S</div>
-          <div className="sizeList-item">M</div>
-          <div className="sizeList-item">L</div>
-          <div className="sizeList-item">XL</div>
-          <div className="sizeList-item">XXL</div>
+        <div className="display-right-price">
+          <div className="rightPriceOld">Rs {product.old_price}</div>
+          <div className="rightPriceNew">Rs {product.new_price}</div>
         </div>
-      </div>
 
-      <button>ADD TO CART</button>
-      <p className="displayCategory">
-        <span>Category :</span>Women , T-shirt, Crop Top{" "}
-      </p>
-      <p className="displayCategory">
-        <span>Tags :</span>Modern , Latest{" "}
-      </p>
+        <div className="display-right-description">
+          <p>{product.description}</p>
+        </div>
+
+        <div className="display-right-size">
+          <h3>Size</h3>
+          <div className="sizeList">
+            <div className="sizeList-item">S</div>
+            <div className="sizeList-item">M</div>
+            <div className="sizeList-item">L</div>
+            <div className="sizeList-item">XL</div>
+            <div className="sizeList-item">XXL</div>
+          </div>
+        </div>
+
+        <button>ADD TO CART</button>
+        <p className="displayCategory">
+          <span>Category :</span>Women , T-shirt, Crop Top{" "}
+        </p>
+        <p className="displayCategory">
+          <span>Tags :</span>Modern , Latest{" "}
+        </p>
+      </div>
     </div>
   );
 };
