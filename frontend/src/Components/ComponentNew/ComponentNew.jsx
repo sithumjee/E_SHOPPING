736a1f08a5e8cbcadc2df4696005eb093aeb884c
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-06 00:43:05 +0530

    comments

--- frontend/src/Components/ComponentNew/ComponentNew.jsx
+++ frontend/src/Components/ComponentNew/ComponentNew.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "./ComponentNew.css";
-import Items from "../Items/Items";
-import new_collections from "../Assets/new_collections";
+import Items from "../Items/Items"; // Importing the Items component
+import new_collections from "../Assets/new_collections"; // Importing the new collections data
 
 const ComponentNew = () => {
   return (
     <div className="newComponent">
-      <h1>LATEST ARRIVALS</h1>
-      <hr />
+      <h1>LATEST ARRIVALS</h1> {/* Heading for the latest arrivals */}
+      <hr /> {/* Horizontal line */}
       <div className="new-item">
         {new_collections.map((item, i) => {
+          // Mapping through the new collections data
           return (
             <Items
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              new_price={item.new_price}
-              old_price={item.old_price}
+              key={i} // Unique key for each item
+              id={item.id} // Item ID
+              name={item.name} // Item name
+              image={item.image} // Item image
+              new_price={item.new_price} // New price of the item
+              old_price={item.old_price} // Old price of the item
             />
           );
         })}
