c77d89a9c9278041dc00775f0b996cac11d1fefb
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-01-06 18:45:21 +0530

    shopcontext

--- frontend/src/Context/ShopContent.jsx
+++ frontend/src/Context/ShopContent.jsx
@@ -3,8 +3,29 @@ import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length; index++) {
+    cart[index] = 0;
+  }
+};
+//
 const ShopContextProvider = (props) => {
-  const contextValue = { all_product };
+  //
+  const [cartItems, setCartItems] = React.useState(getDefaultCart());
+
+  //
+  //
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const removeToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+  //
+  //
+  const contextValue = { all_product, cartItems, addToCart, removeToCart };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
