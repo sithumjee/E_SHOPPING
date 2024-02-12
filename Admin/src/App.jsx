cb9abdf70b7a4798e2bb5c95719a499d5a714349
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2024-02-12 13:46:06 +0530

    Sidebar componenet

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,10 +1,12 @@
-// import React from 'react'
+import React from "react";
 
+import Navbar from "./Components/NAvbar/Navbar";
 import Admin from "./Pages/Admin/Admin";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Admin />
     </div>
   );
--- Admin/src/Pages/Admin/Admin.jsx
+++ Admin/src/Pages/Admin/Admin.jsx
@@ -1,11 +1,11 @@
-// import React from "react";
+import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 import "./Admin.css";
-import Navbar from "../../Components/NAvbar/Navbar";
 
 const Admin = () => {
   return (
     <div className="admin">
-      <Navbar />
+      <Sidebar />
     </div>
   );
 };
